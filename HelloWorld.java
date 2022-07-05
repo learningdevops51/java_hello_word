426d4426ba8daf468cb0d4e581240eed957f99cf
learningdevops51 <learningdevops51@gmail.com>
2022-07-05 16:14:12 +0000

    poll scm

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("This is MY first project, I have added second line");
+                System.out.println("This is poll SCM line");
 	}
 }
