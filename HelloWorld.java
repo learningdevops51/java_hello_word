d9b26d0c8132316a856af5ca2e07e42475424953
learningdevops51 <learningdevops51@gmail.com>
2022-07-05 15:06:45 +0000

    secondline

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("This is MY first project");
+		System.out.println("This is MY first project, I have added second line");
 	}
 }
