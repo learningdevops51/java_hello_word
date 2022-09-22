d06a0dcbc38d4b39dc4e19192ff8e18698a1e759
learningdevops51 <105651633+learningdevops51@users.noreply.github.com>
2022-09-23 01:21:50 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("This is MY first project, I have added second line");
-                System.out.println("This is poll SCM line setup for every min");
-	}
-}
