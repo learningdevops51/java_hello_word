b88ed9c6c29f374e2fb5de48b8fddcd91382a6f7
learningdevops51 <105651633+learningdevops51@users.noreply.github.com>
2022-09-23 02:30:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
+# Configure the AWS Provider
 provider "aws" {
-  profile = "terraform"
-  region  = "us-east-1"
+  region = "us-east-1"
   access_key = "AKIAQQBQ2WVVNLQZFBW4"
   secret_key = "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"
 }
